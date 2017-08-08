c9f6f7f4b4ee1eb8bd4528ccfcbab55899515b9a
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-08-08 11:11:48 -0700

    Add Post and Comment models with migrations and seeders

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,7 +12,12 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
+
+$factory->define(User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
@@ -20,5 +25,31 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(Post::class, function (Faker\Generator $faker) {
+
+    return [
+        'title' => $faker->sentence,
+        'body' => $faker->realText(2000),
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        }
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(Comment::class, function (Faker\Generator $faker) {
+
+    return [
+        'body' => $faker->realText(300),
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
+        'post_id' => function () {
+            return factory(Post::class)->create()->id;
+        }
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    
     /**
      * Run the database seeds.
      *
@@ -12,5 +13,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        collect(factory(\App\Models\User::class, 25)->create())
+            ->each(function ($user) {
+                collect(factory(\App\Models\Post::class, 50)->create(['user_id' => $user->id]))
+                    ->each(function ($post) {
+                        collect(factory(\App\Models\Comment::class, rand(5, 10))->create([
+                            'post_id' => $post->id,
+                            'user_id' => rand(1, 25),
+                        ]));
+                    });
+            });
     }
 }
