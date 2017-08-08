704adf18266d9598a583c0f7e098a155ce5fee4a
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-08-08 16:29:59 -0700

    Add avatar to defaultIncludes

--- app/Transformers/UserTransformers.php
+++ app/Transformers/UserTransformers.php
@@ -10,12 +10,20 @@ use League\Fractal\TransformerAbstract;
 class UserTransformers extends TransformerAbstract
 {
     /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        'avatar',
+    ];
+    
+    /**
      * List of resources possible to include.
      *
      * @var array
      */
     protected $availableIncludes = [
-        'avatar',
         'posts',
     ];
 
